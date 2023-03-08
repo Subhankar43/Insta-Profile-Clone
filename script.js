de616442960502d3d904fb3b819895cac41d8941
Subhankar43 <subhankarkundu43@gmail.com>
2023-03-08 21:34:24 +0530

    added cdn and js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function likeButton() {
+  let heart = document.querySelector(".heart");
+  let likes = document.querySelector(".likes");
+
+  if (heart.src.match("heart.png")) {
+    heart.src = "heart_red.png";
+    likes.innerHTML = "Liked by you and 102 others";
+  } else {
+    heart.src = "heart.png";
+    likes.innerHTML = "102 likes";
+  }
+}
