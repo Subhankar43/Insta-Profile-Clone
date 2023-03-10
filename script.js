7932d65930b56a30a47d74a3ecce654ce084aa9d
Subhankar43 <subhankarkundu43@gmail.com>
2023-03-10 23:38:42 +0530

    minor changes and cdn coming soon

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+
+This code is written By Vortx
+Don't forget to give the credit 😁 
+
+*/
 function likeButton() {
   let heart = document.querySelector(".heart");
   let likes = document.querySelector(".likes");
